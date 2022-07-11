9298652d31b8ee47477ba517b77c87bfbb44c82c
perpetual-pelican <18222948+perpetual-pelican@users.noreply.github.com>
2022-07-10 20:20:26 -0500

    clean up code and debugging messages

--- investment_calculator.py
+++ investment_calculator.py
@@ -1,7 +1,17 @@
+from mimetypes import init
+from logger import log
 
-def calculate_cmpd_int (yearly_investment, apy, years):
+def calculate_cmpd_int (initial_investment, yearly_investment, apy, years):
   int_factor = 1 + apy
-  if years < 1:
-    return 0
-  sub_total = calculate_cmpd_int(yearly_investment, apy, years - 1)
-  return (sub_total + yearly_investment) * int_factor
+  if years <= 1:
+    log.debug("\nCalculating Compound Interest")
+    log.debug("  Initial Investment: $%.2f", initial_investment)
+    log.debug("  Yearly Investment: $%.2f", yearly_investment)
+    log.debug("  APY: %.2f%%", apy * 100)
+    total = initial_investment * int_factor
+    log.debug("  Year %d Total: $%.2f", years, total)
+    return total
+  sub_total = calculate_cmpd_int(initial_investment, yearly_investment, apy, years - 1)
+  total = (sub_total + yearly_investment) * int_factor
+  log.debug("  Year %d Total: $%.2f", years, total)
+  return total
--- tax_calculator.py
+++ tax_calculator.py
@@ -4,9 +4,6 @@ from logger import log
 
 getcontext().rounding = ROUND_HALF_UP
 
-def salary_after_tax(salary, pre_tax_investments=0):
-  return salary - calculate_taxes(salary, pre_tax_investments).total
-
 def calculate_taxes (salary, pre_tax_investments=0):
   fed_tax = calculate_fed_tax(salary, pre_tax_investments)
   ss_tax = calculate_ss_tax(salary)
@@ -20,9 +17,10 @@ def calculate_fed_tax (salary, pre_tax_investments=0):
   total_tax = 0
   lower_limit = 0
 
-  log.debug("\nBase Salary: $%.2f", salary)
-  log.debug("Federal Taxable Income: $%.2f", taxable_income)
-  log.debug("Tax Brackets")
+  log.debug("\nCalculating Federal Tax")
+  log.debug("  Base Salary: $%.2f", salary)
+  log.debug("  Federal Taxable Income: $%.2f", taxable_income)
+  log.debug("  Tax Brackets")
   
   for rate, upper_limit in tax_constants.TAX_BRACKETS.items():
     bracket_tax = 0
@@ -33,9 +31,9 @@ def calculate_fed_tax (salary, pre_tax_investments=0):
     else:
       break
 
-    log.debug("%d%%: $%d - $%d", rate*100, lower_limit, upper_limit)
-    log.debug(" bracket tax: $%.2f", bracket_tax)
-    log.debug(" total tax: $%.2f", total_tax)
+    log.debug("  %d%%: $%d - $%d", rate*100, lower_limit, upper_limit)
+    log.debug("   bracket tax: $%.2f", bracket_tax)
+    log.debug("   total tax: $%.2f", total_tax)
 
     total_tax += bracket_tax
     lower_limit = upper_limit
@@ -52,8 +50,9 @@ def calculate_medicare_tax (salary):
 
 def calculate_state_tax (salary, pre_tax_investments=0):
   taxable_income = calculate_taxable_income(salary, tax_constants.PERSONAL_EXEMPTION, pre_tax_investments)
-  log.debug("\nBase Salary: $%.2f", salary)
-  log.debug("State Taxable Income: $%.2f", taxable_income)
+  log.debug("Calculating State Tax")
+  log.debug("  Base Salary: $%.2f", salary)
+  log.debug("  State Taxable Income: $%.2f", taxable_income)
   state_tax = taxable_income * tax_constants.IL_TAX_RATE
   return round(state_tax, 2)
 
