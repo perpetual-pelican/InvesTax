2b237ad4a955fd495e7f66b0c2fa71c582732878
perpetual-pelican <18222948+perpetual-pelican@users.noreply.github.com>
2024-08-12 21:35:35 -0500

    formatting

--- app/routes.py
+++ app/routes.py
@@ -3,17 +3,19 @@ from app import app
 from app.forms import Form
 from tax_calculator import calculate_taxes
 
+
 @app.route('/')
 @app.route('/index')
 def index():
-  return render_template('index.html', title='Home')
+    return render_template('index.html', title='Home')
+
 
 @app.route('/calculate_tax', methods=['GET', 'POST'])
 def calculate_tax():
-  form = Form()
-  salary = None
-  taxes = None
-  if form.validate_on_submit():
-    salary = form.salary.data
-    taxes = calculate_taxes(salary)
-  return render_template('calculate_tax.html', title='Calculate Tax', salary=salary, taxes=taxes, form=form)
+    form = Form()
+    salary = None
+    taxes = None
+    if form.validate_on_submit():
+        salary = form.salary.data
+        taxes = calculate_taxes(salary)
+    return render_template('calculate_tax.html', title='Calculate Tax', salary=salary, taxes=taxes, form=form)
--- investment_calculator.py
+++ investment_calculator.py
@@ -1,17 +1,18 @@
 from mimetypes import init
 from logger import log
 
-def calculate_cmpd_int (initial_investment, yearly_investment, apy, years):
-  int_factor = 1 + apy
-  if years <= 1:
-    log.debug("\nCalculating Compound Interest")
-    log.debug("  Initial Investment: $%.2f", initial_investment)
-    log.debug("  Yearly Investment: $%.2f", yearly_investment)
-    log.debug("  APY: %.2f%%", apy * 100)
-    total = initial_investment * int_factor
+
+def calculate_cmpd_int(initial_investment, yearly_investment, apy, years):
+    int_factor = 1 + apy
+    if years <= 1:
+        log.debug("\nCalculating Compound Interest")
+        log.debug("  Initial Investment: $%.2f", initial_investment)
+        log.debug("  Yearly Investment: $%.2f", yearly_investment)
+        log.debug("  APY: %.2f%%", apy * 100)
+        total = initial_investment * int_factor
+        log.debug("  Year %d Total: $%.2f", years, total)
+        return total
+    sub_total = calculate_cmpd_int(initial_investment, yearly_investment, apy, years - 1)
+    total = (sub_total + yearly_investment) * int_factor
     log.debug("  Year %d Total: $%.2f", years, total)
     return total
-  sub_total = calculate_cmpd_int(initial_investment, yearly_investment, apy, years - 1)
-  total = (sub_total + yearly_investment) * int_factor
-  log.debug("  Year %d Total: $%.2f", years, total)
-  return total
