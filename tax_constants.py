cacfabb71e47a68850cc47672a60116e9154cdef
perpetual-pelican <18222948+perpetual-pelican@users.noreply.github.com>
2024-08-12 19:46:59 -0500

    update tax brackets for 2024

--- tax_constants.py
+++ tax_constants.py
@@ -2,14 +2,26 @@ from collections import OrderedDict
 from decimal import Decimal
 from math import inf
 
+TAX_YEAR = 2024
+
 # 2022 Tax Brackets
+# TAX_BRACKETS = OrderedDict()
+# TAX_BRACKETS[Decimal("0.1")] = 10275
+# TAX_BRACKETS[Decimal("0.12")] = 41775
+# TAX_BRACKETS[Decimal("0.22")] = 89075
+# TAX_BRACKETS[Decimal("0.24")] = 170050
+# TAX_BRACKETS[Decimal("0.32")] = 215950
+# TAX_BRACKETS[Decimal("0.35")] = 539900
+# TAX_BRACKETS[Decimal("0.37")] = inf
+
+# 2024 Tax Brackets
 TAX_BRACKETS = OrderedDict()
-TAX_BRACKETS[Decimal("0.1")] = 10275
-TAX_BRACKETS[Decimal("0.12")] = 41775
-TAX_BRACKETS[Decimal("0.22")] = 89075
-TAX_BRACKETS[Decimal("0.24")] = 170050
-TAX_BRACKETS[Decimal("0.32")] = 215950
-TAX_BRACKETS[Decimal("0.35")] = 539900
+TAX_BRACKETS[Decimal("0.1")] = 11600
+TAX_BRACKETS[Decimal("0.12")] = 47150
+TAX_BRACKETS[Decimal("0.22")] = 100525
+TAX_BRACKETS[Decimal("0.24")] = 191950
+TAX_BRACKETS[Decimal("0.32")] = 243725
+TAX_BRACKETS[Decimal("0.35")] = 609350
 TAX_BRACKETS[Decimal("0.37")] = inf
 
 STANDARD_DEDUCTION = 12950
